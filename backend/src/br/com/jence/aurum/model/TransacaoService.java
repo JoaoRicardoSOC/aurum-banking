de027862d6155fdad8a8f52823c6e68301738fdf
Caio Ribeiro <cmribeiro2007@gmail.com>
2026-05-21 19:52:44 -0300

    feat: adicionar TransacaoService para lidar com operacoes de venda e compra de criptoativos

---
+++ backend/src/br/com/jence/aurum/model/TransacaoService.java
@@ -0,0 +1,73 @@
+package br.com.jence.aurum.model;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class TransacaoService {
+
+    public Transacao realizarCompra(Carteira carteira, Criptoativo criptoativo, BigDecimal valorBrl) {
+        if (carteira.getSaldoDisponivelBrl().compareTo(valorBrl) < 0) {
+            throw new IllegalStateException("Saldo insuficiente");
+        }
+
+        BigDecimal precoAtual = criptoativo.getPrecoAtualBrl();
+        BigDecimal quantidade = valorBrl.divide(precoAtual, 8, RoundingMode.HALF_UP);
+
+        carteira.sacarBrl(valorBrl);
+
+        carteira.atualizarPosicaoCripto(
+                criptoativo,
+                quantidade,
+                true
+        );
+
+        Transacao transacao = new Transacao(
+                1L,
+                Transacao.TipoTransacao.COMPRA,
+                valorBrl,
+                quantidade,
+                precoAtual,
+                new BigDecimal("2"),
+                carteira,
+                criptoativo
+        );
+
+        transacao.marcarComoConcluida();
+
+        carteira.registrarTransacao(transacao);
+
+        return transacao;
+    }
+
+    public Transacao realizarVenda(Carteira carteira, Criptoativo criptoativo, BigDecimal valorBrl) {
+        BigDecimal precoAtual = criptoativo.getPrecoAtualBrl();
+        BigDecimal quantidade = valorBrl.divide(precoAtual, 8, RoundingMode.HALF_UP);
+
+        carteira.atualizarPosicaoCripto(
+                criptoativo,
+                quantidade,
+                false
+        );
+
+        BigDecimal valorVenda = quantidade.multiply(precoAtual);
+
+        carteira.depositarBrl(valorVenda);
+
+        Transacao transacao = new Transacao(
+                2L,
+                Transacao.TipoTransacao.VENDA,
+                valorVenda,
+                quantidade,
+                precoAtual,
+                new BigDecimal("2"),
+                carteira,
+                criptoativo
+        );
+
+        transacao.marcarComoConcluida();
+
+        carteira.registrarTransacao(transacao);
+
+        return transacao;
+    }
+}
